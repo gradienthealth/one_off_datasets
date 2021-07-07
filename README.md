ef5be7eedfc088f203fc195aa83ca806231a5d3f
Haimeng Zhang <haimengzhang@Haimengs-iMac.local>
2021-07-07 10:49:09 -0400

    prostateX

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # one_off_datasets
 Ribfrac: https://colab.research.google.com/drive/1RPQ92Ep4N_rB3q2LgXnacVgBD3LBsGmf?usp=sharing
+
+ProstateX: https://colab.research.google.com/drive/1t4tOjefhRqn9AapzF-Jr5qzMH2m0MwBU?usp=sharing
---
+++ prostatex_colab/wasabi/wasabi.py
@@ -0,0 +1,21 @@
+import os
+
+# s3_read_write_only
+os.environ["AWS_ACCESS_KEY_ID"] = "C0ULSR7NUU9V2P763JRG"
+os.environ["AWS_SECRET_ACCESS_KEY"] = "ID6P2shVEyTLWaAadtBFDJgX54UOO3bXwW0UJYEy"
+os.environ["AWS_REGION"] = "us-east-1"
+os.environ["S3_ENDPOINT"] = "s3.us-east-1.wasabisys.com"
+os.environ["S3_USE_HTTPS"] = "1"
+os.environ["S3_VERIFY_SSL"] = "1"
+
+# export AWS_ACCESS_KEY_ID="C0ULSR7NUU9V2P763JRG"
+# export AWS_SECRET_ACCESS_KEY="ID6P2shVEyTLWaAadtBFDJgX54UOO3bXwW0UJYEy"
+# export AWS_REGION="us-east-1"
+# export S3_ENDPOINT="s3.us-east-1.wasabisys.com"
+# export S3_USE_HTTPS="1"
+# export S3_VERIFY_SSL="1"
+
+
+# root user: ouwen.oh@gmail.com
+# sub user: haimeng
+# password: testtest
